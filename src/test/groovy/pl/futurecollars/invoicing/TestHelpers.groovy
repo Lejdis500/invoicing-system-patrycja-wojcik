24f96bf30005082467b25107930ddd9b579f6501
Lejdis500 <119961714+Lejdis500@users.noreply.github.com>
2023-03-07 18:40:13 +0100

    Task2 (#5)
    * workflow
    
    * jacoco
    
    * Task2
    
    * Task2 gradle wrapper
    
    * Task2
    
    * Task2
    
    * Task2
    
    * Task2
    
    * Task2
    
    * Task2
    
    * Task2 - fix
    
    * Task2 - checkstyle fix

---
+++ src/test/groovy/pl/futurecollars/invoicing/TestHelpers.groovy
@@ -0,0 +1,27 @@
+package pl.futurecollars.invoicing
+
+import pl.futurecollars.invoicing.model.Company
+import pl.futurecollars.invoicing.model.Invoice
+import pl.futurecollars.invoicing.model.InvoiceEntry
+import pl.futurecollars.invoicing.model.Vat
+
+import java.time.LocalDate
+
+class TestHelpers {
+
+    static company(int id) {
+        new Company(("id").repeat(10),
+                "ul. Konopnicka 2A/$id 03-287 Warszawa, Polska",
+                "Rechnungen,Finanzen $id Sp. z o.o")
+    }
+
+    static product(int id) {
+
+        new InvoiceEntry("Programming course $id", BigDecimal.valueOf(id * 1000),
+                BigDecimal.valueOf(id * 1000 * 0.08), Vat.VAT_8)
+    }
+
+    static invoice(int id) {
+        new Invoice(LocalDate.now(), company(id), company(id), List.of(product(id)))
+    }
+}
