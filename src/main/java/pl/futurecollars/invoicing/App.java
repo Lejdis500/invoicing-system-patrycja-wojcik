f878365165768f4517008aa0c64e3d258da2ead8
Patrycja <patiwojcik1@gmail.com>
2023-04-01 21:49:41 +0200

    Task3

--- src/main/java/pl/futurecollars/invoicing/App.java
+++ src/main/java/pl/futurecollars/invoicing/App.java
@@ -19,23 +19,6 @@ public class App {
 
   public static void main(String[] args) {
 
-    Database db = new InMemoryDatabase();
-    InvoiceService service = new InvoiceService(db);
-
-    Company buyer = new Company("123-345-467", "ul. Weronicza, 11-227 Kraków", "MojaFima");
-    Company seller = new Company("456-333-123", "ul. Konopnicka 2A/$id 03-287 Warszawa, Polska ", "Rechnungen Finanze");
-
-    List<InvoiceEntry> products = List.of(new InvoiceEntry("Biuro podróży- dookoła świata ", BigDecimal.valueOf(100), BigDecimal.TEN, Vat.VAT_8));
-
-    Invoice invoice = new Invoice(LocalDate.now(), buyer, seller, products);
-
-    int id = service.save(invoice);
-
-    service.getById(id).ifPresent(System.out::println);
-
-    System.out.println(service.getAll());
-
-    service.delete(id);
 
   }
 }
