27f15d4843d25a524152862f5a71933f8b587cc9
Patrycja <patiwojcik1@gmail.com>
2023-04-01 21:54:19 +0200

    Task3

--- src/main/java/pl/futurecollars/invoicing/App.java
+++ src/main/java/pl/futurecollars/invoicing/App.java
@@ -1,16 +1,5 @@
 package pl.futurecollars.invoicing;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-import pl.futurecollars.invoicing.db.Database;
-import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
-import pl.futurecollars.invoicing.model.Company;
-import pl.futurecollars.invoicing.model.Invoice;
-import pl.futurecollars.invoicing.model.InvoiceEntry;
-import pl.futurecollars.invoicing.model.Vat;
-import pl.futurecollars.invoicing.service.InvoiceService;
-
 public class App {
 
   public String getGreeting() {
