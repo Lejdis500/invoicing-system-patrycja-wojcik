7fd443be043fb7cc5ce49ee702cdb813edde0053
Patrycja <patiwojcik1@gmail.com>
2023-10-14 11:54:41 +0200

    Task6

--- src/main/java/pl/futurecollars/invoicing/utils/JsonService.java
+++ src/main/java/pl/futurecollars/invoicing/utils/JsonService.java
@@ -1,6 +1,5 @@
 package pl.futurecollars.invoicing.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
