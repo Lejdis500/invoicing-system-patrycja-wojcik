730a3fe1d9e3bd1e26919224d5f544b9fb56eb74
Patrycja <patiwojcik1@gmail.com>
2023-12-20 18:13:08 +0100

    Task13

--- src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
+++ src/main/java/pl/futurecollars/invoicing/db/file/FileBasedDatabase.java
@@ -15,7 +15,7 @@ import pl.futurecollars.invoicing.utils.JsonService;
 public class FileBasedDatabase implements Database {
 
   private final Path databasePath;
-  private final IdService idService;
+  private final IdService idProvider;
   private final FilesService filesService;
   private final JsonService jsonService;
 
